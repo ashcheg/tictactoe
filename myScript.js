093aff6054540707b7a3b2e315560d9cbcf42c53
ashcheg <alina.shcheg@gmail.com>
2022-07-06 00:21:10 -0400

    mainly written overall structure of JS

--- myScript.js
+++ myScript.js
@@ -1,7 +1,17 @@
+// Game controler module
+const displayController = ({
 
+    return {
+        // need to write functions
+        score: checkScore(),
+        resetGame: resetGame(),
+        choosePlayer: choosePlayer()
+    }
+})();
 
+// Gameboard module
 const Gameboard = (() => {
-    gameBoard = ['O', 'O', 'X', 'O', 'O', 'X', 'O', 'O', 'X'];
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
 
     // render gameboard on screen
     function render() {
@@ -9,15 +19,24 @@ const Gameboard = (() => {
         for (i=0;i<gameBoard.length;i++) {
             let sign = gameBoard[i];
             let div = document.createElement("div");
+            div.classList = "cell"
             div.id = `cell${i}`
             div.innerHTML = sign;
             gameBoardDiv.appendChild(div);
         }
     }
     return {
-        render: render()
+        render: render(),
+        // need to write functions
+        addMark: addMark()
     }
 })();
+
+
+// All event listeners. Need to atach to elements
+document.addEventListener("click", displayController.resetGame);
+document.addEventListener("click", displayController.choosePlayer);
+document.addEventListener("click", Gameboard.addMark);
 
 // Overall Game object - module
 /* 
